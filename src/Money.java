ddb7946f5792cc216e3b703ba048484f6f508eb9
Ranger <57109248+aangelcruzmx@users.noreply.github.com>
2024-04-16 13:37:21 +0200

    Add files via upload
    FIMNALIZADO CON LOS TEST COMPLETOS

--- src/Money.java
+++ src/Money.java
@@ -7,29 +7,35 @@ public class Money {
     final float USDEUR = 0.841815F;
     final float EURGBP = 0.857839F;
     final float GBPEUR = 1.165826F;
+    final float USDGBP = 0.72214F;
+    final float GBPUSD = 1.384773F;
 
     public Money() {
     }
-/*
-    public Money(TipoMoneda tipoMonedaOrigen, TipoMoneda tipoMonedaDestino, float cantidad) {
-        this.tipoMonedaOrigen = tipoMonedaOrigen;
-        this.tipoMonedaDestino = tipoMonedaDestino;
-        this.cantidad = cantidad;
-    }
 
 
- */
+
+
     public double changue (TipoMoneda origen, TipoMoneda destino, float cantidad){
         float resultado = 0F;
 
         if (origen == TipoMoneda.EUR && destino == TipoMoneda.USD && cantidad > 0){
             return resultado = EURUSD * cantidad;
-        } else if (origen == TipoMoneda.USD && destino == TipoMoneda.EUR && cantidad > 0) {
+        }
+            else if (origen == TipoMoneda.USD && destino == TipoMoneda.EUR && cantidad > 0) {
             return resultado = USDEUR * cantidad;
-        } else if (origen == TipoMoneda.EUR && destino == TipoMoneda.GBP && cantidad > 0){
+        }
+            else if (origen == TipoMoneda.EUR && destino == TipoMoneda.GBP && cantidad > 0){
             return resultado = EURGBP * cantidad;
-        } else if (origen == TipoMoneda.GBP && destino == TipoMoneda.EUR && cantidad > 0) {
+        }
+            else if (origen == TipoMoneda.GBP && destino == TipoMoneda.EUR && cantidad > 0) {
             return resultado = GBPEUR * cantidad;
+        }
+            else if (origen == TipoMoneda.USD && destino == TipoMoneda.GBP && cantidad > 0) {
+            return resultado = USDGBP * cantidad;
+        }
+            else if (origen == TipoMoneda.GBP && destino == TipoMoneda.USD && cantidad > 0) {
+            return resultado = GBPUSD * cantidad;
         }else {
             return -1;
         }
--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -21,6 +21,26 @@ class MoneyTest {
     void changue4() {
         assertEquals( 37.51F, new Money().changue(TipoMoneda.USD,TipoMoneda.EUR, 44.56F), 0.01);
     }
+    @Test
+    void changue5() {
+        assertEquals( 138.49F, new Money().changue(TipoMoneda.GBP,TipoMoneda.USD, 100.0F), 0.02);
+    }
+    @Test
+    void changue6() {
+        assertEquals( 722.14F, new Money().changue(TipoMoneda.USD,TipoMoneda.GBP, 1000.0F), 0.01);
+    }
+    @Test
+    void changue7() {
+        assertEquals( -1F, new Money().changue(TipoMoneda.PTS,TipoMoneda.EUR, 100.0F), 0.01);
+    }
+    @Test
+    void changue8() {
+        assertEquals( -1F, new Money().changue(TipoMoneda.EUR,TipoMoneda.PTS, 123.3F), 0.01);
+    }
+    @Test
+    void changue9() {
+        assertEquals( -1F, new Money().changue(TipoMoneda.USD,TipoMoneda.EUR, -167.34F), 0.01);
+    }
 
 
 

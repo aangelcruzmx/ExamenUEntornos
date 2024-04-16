9846a9cc87c92ae5cf223676836ee69ca33420f9
Ranger <57109248+aangelcruzmx@users.noreply.github.com>
2024-04-16 13:21:19 +0200

    Add files via upload
    CREANDO NUEVAS TEST

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -9,6 +9,20 @@ class MoneyTest {
         assertEquals( 28.37F, new Money().changue(TipoMoneda.EUR,TipoMoneda.USD, 23.88F), 0.01);
     }
 
+    @Test
+    void changue2() {
+        assertEquals( 1165.83F, new Money().changue(TipoMoneda.GBP,TipoMoneda.EUR, 1000.0F), 0.01);
+    }
+    @Test
+    void changue3() {
+        assertEquals( 201.21F, new Money().changue(TipoMoneda.EUR,TipoMoneda.GBP, 234.56F), 0.01);
+    }
+    @Test
+    void changue4() {
+        assertEquals( 37.51F, new Money().changue(TipoMoneda.USD,TipoMoneda.EUR, 44.56F), 0.01);
+    }
+
+
 
 
 }
